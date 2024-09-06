337c322ca1f4a3bf661a90493723694bbea37b95
unknown <adrian.reybrea@dxc.com>
2024-09-06 14:50:12 +0200

    SpringConfig

--- src/main/java/com/banana/bananawhatsapp/config/SpringConfig.java
+++ src/main/java/com/banana/bananawhatsapp/config/SpringConfig.java
@@ -4,11 +4,14 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@ComponentScan(basePackages = {"com.banana.bananawhatsapp.persistencia","com.banana.bananawhatsapp.servicios"})
+@ComponentScan(basePackages = {"com.banana.bananawhatsapp.persistencia","com.banana.bananawhatsapp.servicios","com.banana.bananawhatsapp.controladores"})
 @EntityScan(basePackages = {"com.banana.bananawhatsapp.modelos"})
 @Configuration
 @PropertySource("classpath:application.properties")
+@EnableJpaRepositories(basePackages = {"com.banana.bananawhatsapp.persistencia"})
+
 public class SpringConfig {
 
 }
