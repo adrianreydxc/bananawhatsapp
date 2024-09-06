c6b26cd5cab0ce72c50f384d55a6e5f21ce971cc
Daniel Contreeras <dacm.dev@icloud.com>
2024-09-06 13:38:24 +0200

    user repository jpa

--- src/main/java/com/banana/bananawhatsapp/persistencia/IUsuarioRepository.java
+++ src/main/java/com/banana/bananawhatsapp/persistencia/IUsuarioRepository.java
@@ -1,18 +1,15 @@
 package com.banana.bananawhatsapp.persistencia;
 
 import com.banana.bananawhatsapp.modelos.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.sql.SQLException;
 import java.util.Set;
 
-public interface IUsuarioRepository {
-    public Usuario obtener(int id) throws SQLException;
-    public Usuario crear(Usuario usuario) throws SQLException;
+public interface IUsuarioRepository extends JpaRepository<Usuario,Integer> {
 
-    public Usuario actualizar(Usuario usuario) throws SQLException;
-
-    public boolean borrar(Usuario usuario) throws SQLException;
-
+    @Query("Select ")
     public Set<Usuario> obtenerPosiblesDestinatarios(Integer id, Integer max) throws SQLException;
 
 }
